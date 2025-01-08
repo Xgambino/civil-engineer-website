895569e41fad5e057720b3a6d06cf11731879c4e
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2025-01-08 12:12:02 +0300

    `Update dependencies and refactor routes in React app`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Use createRoot from react-dom/client
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );

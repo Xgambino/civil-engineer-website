180001d108f419ef4cc6ab88f1fde73468b86943
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2025-01-10 22:51:15 +0300

    `Added HeroImg2 component to About, Contact, and Project routes and updated their respective files`

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Navbar /> 
+      <HeroImg2 heading="ABOUT." text="Innovative and detail-oriented Civil and Structural Engineer with experience in infrastructure design, construction management, and project coordination. Skilled in the use of engineering tools such as AutoCAD and Civil 3D, with a strong passion for building infrastructure design and sustainable urban development. Proven ability to deliver high-quality, on-time, and cost-effective solutions in multidisciplinary environments, contributing to Africa's infrastructure transformation."/>
+      <Footer />
+    </div>
   )
 }
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/Navbar";
+import HeroImg2 from "../components/HeroImg2";
 import Footer from "../components/Footer";
 const Contact = () => {
   return (
     <div>
       <Navbar /> 
+      <HeroImg2 heading="CONTACT ME" text="Let's have a chat"/>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

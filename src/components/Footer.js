5566f804f0bb04b46903a1c3255085fbb8f842e0
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2025-01-10 19:39:00 +0300

    `Added Footer component and styles, updated HeroImg and Navbar styles`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,77 @@
-import React from 'react'
-impo
+import React from "react";
+import "./FooterStyles.css";
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaTwitter,
+  FaPhone
+} from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>233,Tsavo Royal Suburbs.</p>
+              <p>Off Thika Road, Nairobi.</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />{" "}
+              +254 768 761 105
+            </h4>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />{" "}
+              +254 726 795 074
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />{" "}
+              maryannnamutebi@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About Myself</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;

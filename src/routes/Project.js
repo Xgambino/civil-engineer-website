b59e04fb5faf5fd09eb544738812145538e46628
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2025-01-10 19:44:14 +0300

    `Added Navbar and Footer components to Contact and Project routes`

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 const Project = () => {
   return (
-    <div>Project</div>
-  )
-}
+    <div>
+      <Navbar /> 
+      <Footer />
+    </div>
+  );
+};
 
-export default Project
+export default Project;
